edb316afabb9fa21e700dd9eb6fcee34dd067f99
Fonzo <viktorchistyakov27@gmail.com>
2020-11-10 19:52:42 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
 "use strict";
+// 1)
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const result = confirm("Are you here?");
-//console.log(result);
+// 2)
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const answer = prompt("Вам есть 18?", "18");
-console.log(typeof(answer));
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = +prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = +prompt('На сколько оцените его?', '');
+    
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
